28db9f3edb0f3a325d53cb4d37c731e3def60621
Joe Development <146123038+Joe-Development@users.noreply.github.com>
2023-12-03 06:14:09 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -13,6 +13,6 @@ end)
 
 Citizen.CreateThread(function()
 	Citizen.Trace("\nAdding Chat Suggestions for Badger-911 \n")
-	TriggerEvent('chat:addSuggestion', '/911', '[STAFF ONLY] Announcement to Server', {{ name="Information", help="The Information for the 911 Call"}})
+	TriggerEvent('chat:addSuggestion', '/911', 'Make a 911 call', {{ name="Information", help="The Information for the 911 Call"}})
 	TriggerEvent('chat:addSuggestion', '/resp', '[LEO ONLY] respond to the 911 call', {{ name="CallID", help="The Call ID to respond too"}})
 end)
