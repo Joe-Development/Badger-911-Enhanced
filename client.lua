d35c38c658c1cb9e4b2f2c6fb03e63c6daf3a002
Vexy <125819961+VexyDevelopment@users.noreply.github.com>
2023-12-04 21:06:42 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,5 +1,5 @@
 Citizen.CreateThread(function()
-	TriggerServerEvent('Badger-911:CheckPerms')
+	TriggerServerEvent('Badger-911:CheckPerms') -- nigga this is gonna kill performance
 end)
 
 RegisterNetEvent("Badger-911:SetWaypoint")
@@ -7,12 +7,12 @@ AddEventHandler("Badger-911:SetWaypoint", function(x, y)
 	-- Set the waypoint for this player
 	SetNewWaypoint(x, y)
 	-- DEBUG: un comment the print statement below for debugging
-	-- print(x, y)
+	-- print(x, y) - remove this debug shit your ass
 end)
 
 
 Citizen.CreateThread(function()
 	Citizen.Trace("\nAdding Chat Suggestions for Badger-911 \n")
-	TriggerEvent('chat:addSuggestion', '/911', 'Make a 911 call', {{ name="Information", help="The Information for the 911 Call"}})
-	TriggerEvent('chat:addSuggestion', '/resp', '[LEO ONLY] respond to the 911 call', {{ name="CallID", help="The Call ID to respond too"}})
+	TriggerEvent('chat:addSuggestion', '/911', 'Make a 911 call', {{ name="Information", help="The Information for the 911 Call"}}) -- Add Config Here nigga
+	TriggerEvent('chat:addSuggestion', '/resp', '[LEO ONLY] respond to the 911 call', {{ name="CallID", help="The Call ID to respond too"}}) -- This Shit  is ass
 end)
